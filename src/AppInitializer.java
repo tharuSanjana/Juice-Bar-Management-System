cee040e6610999a652590959923fe009e82c65ac
tharuSanjana <tharushisanjana28@gmail.com>
2025-10-26 23:57:20 +0530

    create interfaces and navigate them

---
+++ src/AppInitializer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class AppInitializer {
+    public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(() -> new LoginForm().setVisible(true));
+        
+    }
+    
+}
